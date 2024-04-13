667920510824e3de3c565aef77049f170e673915
CatB1t <skytedits@gmail.com>
2024-04-13 16:33:04 +0200

    Add Logger

---
+++ src/Utils/Logger.cpp
@@ -0,0 +1,60 @@
+#include "Logger.h"
+
+#include <format>
+
+namespace aker {
+	Logger::Logger(const std::string& prefix)
+		: prefix_(prefix) 
+	{}
+
+	void Logger::Info(const char* format, ...)
+	{
+		va_list args;
+		va_start(args, format);
+		Log_(Level::kInfo, format, args);
+		va_end(args);
+	}
+
+	void Logger::Warning(const char* format, ...)
+	{
+		va_list args;
+		va_start(args, format);
+		Log_(Level::kWarning, format, args);
+		va_end(args);
+	}
+
+	void Logger::Error(const char* format, ...)
+	{
+		va_list args;
+		va_start(args, format);
+		Log_(Level::kError, format, args);
+		va_end(args);
+	}
+
+	void Logger::Log_(Level level, const char* format, va_list args) 
+	{
+		std::string level_str;
+		FILE* stream;
+		switch (level)
+		{
+		case Level::kInfo:
+			level_str = "INFO";
+			stream = stdout;
+			break;
+		case Level::kWarning:
+			level_str = "WARNING";
+			stream = stdout;
+			break;
+		case Level::kError:
+			level_str = "ERROR";
+			stream = stderr;
+			break;
+		default:
+			fprintf(stderr, "LOGGER::ERROR: Unknown level");
+			return;
+		}
+
+		std::string new_format = std::format("{}::{}: {}\n", prefix_.c_str(), level_str.c_str(), format);
+		vfprintf(stream, new_format.c_str(), args);
+	}
+}
---
+++ src/Utils/Logger.h
@@ -0,0 +1,29 @@
+#ifndef UTILS_LOGGER_H_
+#define UTILS_LOGGER_H_
+
+#include <cstdarg>
+#include <string>
+
+namespace aker {
+
+	class Logger {
+	public:
+		Logger(const std::string& prefix);
+
+		void Info(const char* format, ...);
+		void Warning(const char* format, ...);
+		void Error(const char* format, ...);
+
+	private:
+		enum class Level {
+			kInfo,
+			kWarning,
+			kError
+		};
+
+		std::string prefix_;
+		void Log_(Level level, const char* format, va_list args);
+	};
+
+}
+#endif
