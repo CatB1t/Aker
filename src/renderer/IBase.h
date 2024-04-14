2a34fe1ff7077c42a93cc345913d7ae8d1bcb5f7
CatB1t <skytedits@gmail.com>
2024-04-14 18:57:10 +0200

    Add bind and unbind methods to IBase

--- src/renderer/IBase.h
+++ src/renderer/IBase.h
@@ -7,6 +7,8 @@ namespace aker
 	public:
 		IBase() {};
 		virtual ~IBase() {};
+		virtual void Bind() const = 0;
+		virtual void Unbind() const = 0;
 		unsigned int GetId() const { return opengl_id_; };
 	private:
 		unsigned int opengl_id_ = -1;
